163605ed8b5b3dc7a8de6b981e17aa30bd94d07d
Gergő Móricz <mo.geryy@gmail.com>
2017-07-15 14:14:03 +0200

    Fixed code linting issues

--- index.js
+++ index.js
@@ -4,29 +4,21 @@
  * @returns {Number} Checksum, -1 if unknown parameter
  */
 function calculate(data){
-    var checksum = 0x00;
-    if (Array.isArray(data)){
-        data.forEach(function(number){
-            checksum += number;
-        });
-    } else {
-        if (!Number.isNaN(data)){
-            if (Number.isInteger(data)){
-                if (data >= 0){
-                    checksum = data;
-                } else {
-                    return -1;
-                }
-            } else {
-                return -1;
-            }
-        } else {
-            return -1;
-        }
-    }
-    checksum %= 128;
-    checksum = 128 - checksum;
-    return checksum;
-};
+	var checksum = 0x00;
+	
+	if (Array.isArray(data)){
+		data.forEach(function(number){
+			checksum += number;
+		});
+	} else if (!Number.isNaN(data) && Number.isInteger(data) && data >= 0) {
+		checksum = data;
+	} else {
+		return -1;
+	}
+
+	checksum %= 128;
+	checksum = 128 - checksum;
+	return checksum;
+}
 
 module.exports = calculate;
--- test/test.js
+++ test/test.js
@@ -1,24 +1,24 @@
-var assert = require('assert');
-var RolandChecksum = require('./../index.js')
-describe('RolandChecksum()', function(){
-    it('should return a valid checksum when an array of numbers are passed', function(){
-        assert.equal(0x20, RolandChecksum([0x20, 0x00, 0x00, 0x74, 0x65, 0x73, 0x74]));
-    });
-    it('should return a valid checksum when a number is passed', function(){
-        assert.equal(0x60, RolandChecksum(0x20));
-    });
-    it('should return -1 when a non-number value is passed', function(){
-        assert.equal(-1, RolandChecksum("hello"));
-        assert.equal(-1, RolandChecksum(true));
-        assert.equal(-1, RolandChecksum(NaN));
-    });
-    it('should return -1 when a non-integer number is passed', function(){
-        assert.equal(-1, RolandChecksum(0.1));
-        assert.equal(-1, RolandChecksum(Math.PI));
-        assert.equal(-1, RolandChecksum(Infinity));
-        assert.equal(-1, RolandChecksum(-Infinity));
-    });
-    it('should return -1 when a negative number is passed', function(){
-        assert.equal(-1, RolandChecksum(-1));
-    });
+var assert = require("assert");
+var rolandChecksum = require("./../index.js");
+describe("rolandChecksum()", function(){
+	it("should return a valid checksum when an array of numbers are passed", function(){
+		assert.equal(0x20, rolandChecksum([0x20, 0x00, 0x00, 0x74, 0x65, 0x73, 0x74]));
+	});
+	it("should return a valid checksum when a number is passed", function(){
+		assert.equal(0x60, rolandChecksum(0x20));
+	});
+	it("should return -1 when a non-number value is passed", function(){
+		assert.equal(-1, rolandChecksum("hello"));
+		assert.equal(-1, rolandChecksum(true));
+		assert.equal(-1, rolandChecksum(NaN));
+	});
+	it("should return -1 when a non-integer number is passed", function(){
+		assert.equal(-1, rolandChecksum(0.1));
+		assert.equal(-1, rolandChecksum(Math.PI));
+		assert.equal(-1, rolandChecksum(Infinity));
+		assert.equal(-1, rolandChecksum(-Infinity));
+	});
+	it("should return -1 when a negative number is passed", function(){
+		assert.equal(-1, rolandChecksum(-1));
+	});
 });
