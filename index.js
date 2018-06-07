3ad3090b09ed2ad9be8540eabd0b6fb48d84699b
Gergő Móricz <mo.geryy@gmail.com>
2018-06-07 20:30:58 +0200

    Fixed a doc style inconsistency.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Returns the calculated checksum from the passed data
  * @param {(Number[]|Number)} data The array of numbers to checksum
- * @returns {Number} Checksum
+ * @returns {Number} The checksum
  */
 module.exports = function (data) {
 	var checksum = Array.isArray(data) ? data.reduce((a, b) => a + b, 0) : data; // If input is array, sum array. Otherwise, use input.
